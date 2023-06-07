7aef8b1d938e395ebdb6c60faa59baa5654138b0
AnhKhoa <khoa.mak246@gmail.com>
2023-06-07 15:33:07 +0700

    Moe

---
+++ src/main/java/com/dsecurity/DSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.dsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsecurity/DSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
